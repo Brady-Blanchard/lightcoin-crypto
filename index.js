eaedc75f5379ac681a46093a9f7ae442cd7e750c
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-12 11:56:09 -0300

    Add: Classes and sub classes for banking

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance () {
+    let totalBalance = 0;
+    for (const transaction of this.transactions) {
+      totalBalance += transaction.value;
+    }
+    return totalBalance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+  
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+
+}
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    return true
   }
 
 }
 
 
-
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('snow-patrol');
 
-t2 = new Withdrawal(9.99);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Withdrawal(50.00, myAccount);
+console.log('Withdrawal:', t1.amount);
+console.log('Account Balance:', myAccount.balance);
+
+const t2 = new Deposit(120.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Deposit:', t2.amount);
+console.log('Account Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(50.00, myAccount);
+t3.commit();
+console.log('Withdrawal:', t3.amount);
+console.log('Ending Balance:', myAccount.balance);
+console.log('Transaction History:', myAccount.transactions);
